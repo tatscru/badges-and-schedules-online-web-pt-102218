65bfb0812e45134c85f1a05bc1242e0554e65041
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-25 16:23:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,6 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each { |badge| puts badge}
-  assign_rooms(attendees).each { |room_number| puts room_number}
+  assign_rooms(attendees).each { |room| puts room}
 end 
    
