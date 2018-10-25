260f497f0ff03bb3b8c68e531d79e57875bab18a
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-25 14:56:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,11 +5,11 @@ def badge_maker(name)
 end
   
 def batch_badge_creator(names)
-  badges = []
-  names.each do |name| 
-    badges << "Hello, my name is #{name}."
-  end 
-  badges
+  # badges = []
+  # names.each do |name| 
+  #   badges << "Hello, my name is #{name}."
+  # end 
+  # badges
 end
     # {|names| puts "Hello, my name is #{name}."}
 
