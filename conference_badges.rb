ab1a76dd189fd3d4bd1d626b4407123345d4ae6e
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-25 14:56:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name) 
+  "Hello, my name is #{name}." 
+end
+  
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name| 
+    badges << "Hello, my name is #{name}."
+  end 
+  badges
+end
+    # {|names| puts "Hello, my name is #{name}."}
+
+
+# def assign_rooms(name)
+#   "Hello, #{name}! You'll be assigned to room #{number}!"]
+# end 
+
+# badge_maker("")
+
+# each_with_index 
+
+
+# def printer(attendees)
+# end 
+  
